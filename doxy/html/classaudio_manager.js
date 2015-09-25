a03612b0fde04ea4e89dc2c809d95c80edad9847
Jeroen Mathon <deathslycer@gmail.com>
2015-09-25 11:14:13 +0200

    Added doxygen documentation.

---
+++ doxy/html/classaudio_manager.js
@@ -0,0 +1,10 @@
+var classaudio_manager =
+[
+    [ "PLAY", "classaudio_manager.html#ab797eef674719ab555f3bc0da5f3e8e9a51ad40959f0b1ca4790c7f140d23b966", null ],
+    [ "PAUSE", "classaudio_manager.html#ab797eef674719ab555f3bc0da5f3e8e9a7bc1c088652f7c32fd67a9902b05b451", null ],
+    [ "STOP", "classaudio_manager.html#ab797eef674719ab555f3bc0da5f3e8e9a2574641d1ef506923f8fcbe7fafbdc17", null ],
+    [ "audioManager", "classaudio_manager.html#ac33c12e5070a2fb80fce9f231999995d", null ],
+    [ "~audioManager", "classaudio_manager.html#ab6a24a54c7e897c0d872f6a219a48728", null ],
+    [ "setState", "classaudio_manager.html#a27cdb7486e1793df14a42fed62aaf673", null ],
+    [ "setStream", "classaudio_manager.html#ae8e0e3c82b018fda14157b1169bf76e3", null ]
+];
